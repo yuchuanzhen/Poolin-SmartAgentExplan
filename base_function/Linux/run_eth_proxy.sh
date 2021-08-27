a7696098d807d768065fe3a14f4029854c51027f
ruyi.chang <ruyi.chang@blockin.com>
2021-08-27 14:13:06 +0800

    添加windows和linux代理基本功能运行脚本

---
+++ base_function/Linux/run_eth_proxy.sh
@@ -0,0 +1,50 @@
+set -ex
+#必填
+host_coin=ETH
+
+#选填
+proxy_port=
+docker_name=
+
+###内部参数处理###
+host_coin=`echo $host_coin| tr 'a-z' 'A-Z'`
+#  docker_name 未填写则默认 proxy_$$host_coin
+if [ -z "${docker_name}" ]; then
+  docker_name=proxy_`echo $host_coin| tr 'A-Z' 'a-z'`
+  echo "docker_name not set, default value: ${docker_name}"
+fi
+
+# proxy_port  未填写则默认以下值
+f [ -z "${proxy_port}" ]; then
+        case $host_coin in
+                "BTC")
+                        proxy_port=8001
+                        ;;
+                "ETH")
+                        proxy_port=8005
+                        ;;
+                *)
+                        echo "$host_coin not support by proxy yet!"
+                        exit 1;
+                        ;;
+        esac
+
+  echo "proxy_port not set, default value: ${proxy_port}"
+fi
+
+docker_version=registry.cn-beijing.aliyuncs.com/poolin_public/proxy:latest
+docker pull ${docker_version}
+
+docker stop -t 3 ${docker_name} || /bin/true
+docker rm ${docker_name} || /bin/true
+
+docker run -it --restart always -d \
+        --dns 119.29.29.29 \
+        --dns 223.5.5.5 \
+        --privileged=true \
+        --env HOST_COIN=${host_coin} \
+        --log-opt mode=non-blocking --log-opt max-buffer-size=4m --log-driver journald \
+        -v /work:/work \
+        --name ${docker_name} \
+        -p ${proxy_port}:1801 \
+        ${docker_version}
